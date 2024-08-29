1be4a25e3af81886078f0c3ab84f11f3cf990b66
sindhujaSirigeri <132906670+sindhujaSirigeri@users.noreply.github.com>
2024-08-29 15:39:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ st.sidebar.write("Dataset from Kaggle: [Fake News Classification](https://www.ka
 st.sidebar.write("The dataset was published in the following paper:\n- **IEEE Transactions on Computational Social Systems**: pp. 1-13 (doi: [10.1109/TCSS.2021.3068519](https://doi.org/10.1109/TCSS.2021.3068519)).")
 
 st.sidebar.header("Project Repository") 
-st.sidebar.write("The GitHub repository with all the code and details on this project can be found at:\n- [Project-4](https://github.com/Whereiswendy/Project-4)\n- [Fake_vs_fact_Predictor](https://github.com/sindhujaSirigeri/Fake_vs_fact_Predictor)")
+st.sidebar.write("The GitHub repository with all the code and details on this project can be found at:\n- [Project-4](https://github.com/Whereiswendy/Project-4)\n- [Fake-News-Predictor-App](https://github.com/sindhujaSirigeri/Fake-News-Predictor-App)")
 
 # Define the preprocess_input function
 def preprocess_input(title, text):
